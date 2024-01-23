11e2999ed63f42d9e5b2e5565a83e0ce3df2bc39
11jerry26 <2529498322@qq.com>
2024-01-23 19:55:48 +0800

    合并分支

---
+++ src/views/teacher/teacher/index.vue
@@ -0,0 +1,385 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="工号" prop="teacherId">
+        <el-input
+          v-model="queryParams.teacherId"
+          placeholder="请输入工号"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="姓名" prop="name">
+        <el-input
+          v-model="queryParams.name"
+          placeholder="请输入姓名"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="学校" prop="school">
+        <el-input
+          v-model="queryParams.school"
+          placeholder="请输入学校"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="教研组" prop="subject">
+        <el-select v-model="queryParams.subject" placeholder="请选择教研组" clearable>
+          <el-option
+            v-for="dict in dict.type.teaching_group"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="角色" prop="role">
+        <el-select v-model="queryParams.role" placeholder="请选择角色" clearable>
+          <el-option
+            v-for="dict in dict.type.teacher_role"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['teacher:teacher:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['teacher:teacher:edit']"
+        >修改</el-button>
+      </el-col>
+<!--      <el-col :span="1.5">-->
+<!--        <el-button-->
+<!--          type="danger"-->
+<!--          plain-->
+<!--          icon="el-icon-delete"-->
+<!--          size="mini"-->
+<!--          :disabled="multiple"-->
+<!--          @click="handleDelete"-->
+<!--          v-hasPermi="['teacher:teacher:remove']"-->
+<!--        >删除</el-button>-->
+<!--      </el-col>-->
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['teacher:teacher:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="teacherList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="工号" align="center" prop="teacherId" />
+      <el-table-column label="姓名" align="center" prop="name" />
+      <el-table-column label="邮箱" align="center" prop="email" />
+      <el-table-column label="电话" align="center" prop="phoneNumber" />
+      <el-table-column label="学校" align="center" prop="school" />
+      <el-table-column label="教研组" align="center" prop="subject">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.teaching_group" :value="scope.row.subject"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="角色" align="center" prop="role">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.teacher_role" :value="scope.row.role"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="账号状态" align="center" prop="status">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.is_using" :value="scope.row.status"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['teacher:teacher:edit']"
+          >修改</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改教师管理对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="form.name" placeholder="请输入姓名" />
+        </el-form-item>
+        <el-form-item label="账号" prop="account">
+          <el-input v-model="form.account" placeholder="请输入账号" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password" placeholder="请输入密码" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="form.email" placeholder="请输入邮箱" />
+        </el-form-item>
+        <el-form-item label="电话" prop="phoneNumber">
+          <el-input v-model="form.phoneNumber" placeholder="请输入电话" />
+        </el-form-item>
+        <el-form-item label="学校" prop="school">
+          <el-input v-model="form.school" placeholder="请输入学校" />
+        </el-form-item>
+        <el-form-item label="教研组" prop="subject">
+          <el-select v-model="form.subject" placeholder="请选择教研组">
+            <el-option
+              v-for="dict in dict.type.teaching_group"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="角色" prop="role">
+          <el-select v-model="form.role" placeholder="请选择角色">
+            <el-option
+              v-for="dict in dict.type.teacher_role"
+              :key="dict.value"
+              :label="dict.label"
+              :value="parseInt(dict.value)"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="账号状态" prop="status">
+          <el-select v-model="form.status" placeholder="请选择账号状态">
+            <el-option
+              v-for="dict in dict.type.is_using"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="性别" prop="sex">
+          <el-select v-model="form.sex" placeholder="请选择性别">
+            <el-option
+              v-for="dict in dict.type.sys_user_sex"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listTeacher, getTeacher, delTeacher, addTeacher, updateTeacher } from "@/api/teacher/teacher";
+
+export default {
+  name: "Teacher",
+  dicts: ['teacher_role', 'is_using', 'teaching_group', 'sys_user_sex'],
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 教师管理表格数据
+      teacherList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        teacherId: null,
+        name: null,
+        school: null,
+        subject: null,
+        role: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        name: [
+          { required: true, message: "姓名不能为空", trigger: "blur" }
+        ],
+        account: [
+          { required: true, message: "账号不能为空", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" }
+        ],
+        subject: [
+          { required: true, message: "教研组不能为空", trigger: "change" }
+        ],
+        role: [
+          { required: true, message: "角色不能为空", trigger: "change" }
+        ],
+        status: [
+          { required: true, message: "账号状态不能为空", trigger: "change" }
+        ],
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询教师管理列表 */
+    getList() {
+      this.loading = true;
+      listTeacher(this.queryParams).then(response => {
+        this.teacherList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        teacherId: null,
+        name: null,
+        account: null,
+        password: null,
+        email: null,
+        phoneNumber: null,
+        school: null,
+        subject: null,
+        role: null,
+        createBy: null,
+        createTime: null,
+        updateBy: null,
+        updateTime: null,
+        avatar: null,
+        delFlag: null,
+        status: null,
+        sex: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.teacherId)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加教师管理";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const teacherId = row.teacherId || this.ids
+      getTeacher(teacherId).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改教师管理";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.teacherId != null) {
+            updateTeacher(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addTeacher(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    // /** 删除按钮操作 */
+    // handleDelete(row) {
+    //   const teacherIds = row.teacherId || this.ids;
+    //   this.$modal.confirm('是否确认删除教师管理编号为"' + teacherIds + '"的数据项？').then(function() {
+    //     return delTeacher(teacherIds);
+    //   }).then(() => {
+    //     this.getList();
+    //     this.$modal.msgSuccess("删除成功");
+    //   }).catch(() => {});
+    // },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('teacher/teacher/export', {
+        ...this.queryParams
+      }, `teacher_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
